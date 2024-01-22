fc95406c6ab6b3aa089a8991881de8950af1861d
AymanAkashi <aaggoujj@student.1337.ma>
2024-01-22 23:52:02 +0100

    feat: seed the database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,23 @@
 import { PrismaClient } from '@prisma/client';
-import { asyncScheduler } from 'rxjs';
 
+// initialize Prisma Client
 const prisma = new PrismaClient();
 
 async function main() {
-  const Post1 = await prisma.article.upsert({
-    where: { title: 'prisma Adds Support For MongoDb' },
+  // create two dummy articles
+  const post1 = await prisma.article.upsert({
+    where: { title: 'Prisma Adds Support for MongoDB' },
     update: {},
     create: {
-      title: 'prisma Adds Support For MongoDb',
-      body: 'prisma Adds Support For MongoDb',
-      description: 'this is the fisrt article for testing the prisma',
+      title: 'Prisma Adds Support for MongoDB',
+      body: 'Support for MongoDB has been one of the most requested features since the initial release of...',
+      description:
+        "We are excited to share that today's Prisma ORM release adds stable support for MongoDB!",
       published: false,
     },
   });
 
-  const Post2 = await prisma.article.upsert({
+  const post2 = await prisma.article.upsert({
     where: { title: "What's new in Prisma? (Q1/22)" },
     update: {},
     create: {
@@ -27,14 +29,16 @@ async function main() {
     },
   });
 
-  console.log({ Post1, Post2 });
-
-  main()
-    .catch((e) => {
-      console.error(e);
-      process.exit(1);
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
-    });
+  console.log({ post1, post2 });
 }
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
