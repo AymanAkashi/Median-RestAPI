87d2be1ac091827a273e196c25a68c75138cb667
AymanAkashi <aaggoujj@student.1337.ma>
2024-01-23 15:44:18 +0100

    feat: setuping user services
    - add authorId field to ArticleEntity
    - implement CRUD endpoints for Users
    - Define the User entity and DTO classes

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+import { Validate } from 'class-validator';
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+
+  password: string;
+}
