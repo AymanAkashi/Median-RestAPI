9e049e9fecb9832082f5a94404c379c824f4a1f1
AymanAkashi <aaggoujj@student.1337.ma>
2024-01-26 10:37:04 +0100

    feat: init frontend Media project

---
+++ app/auth/login/layout.tsx
@@ -0,0 +1,23 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const layout = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) => {
+    const mystyle = {
+        minHeight: 'calc(100vh - 150px)',
+    }
+
+    return (
+        <div className="w-full min-h-screen">
+            <Navbar />
+            <main className="flex justify-center items-center" style={mystyle}>
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default layout
---
+++ components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { GrContact } from 'react-icons/gr'
+import Image from 'next/image'
+
+export default function Navbar() {
+    return (
+        <div className="bg-gray-600/10 w-full h-16 flex justify-between items-center text-neutral-800 font-thin rounded-b-3xl shadow-lg">
+            <div className="p-1 ml-1 w-14 h-14 flex justify-center items-center bg-white/50 rounded-full">
+                <Image
+                    src="/assets/logo.svg"
+                    alt="Picture of the author"
+                    width={100}
+                    height={100}
+                />
+            </div>
+            <div className="flex items-center justify-around">
+                <a href="#" className="mx-2" title="Home">
+                    Home
+                </a>
+                <a href="#" className="mx-2">
+                    About
+                </a>
+                <a
+                    href="#"
+                    className="mx-2 flex justify-center items-center transition-all delay-75 duration-150"
+                >
+                    <GrContact className="inline-block peer hover:text-white transition-all delay-75 duration-150" />
+                    {/* <p className='peer-hover:block hover:block hidden transition-all delay-200 duration-150'>Contact</p> */}
+                </a>
+            </div>
+        </div>
+    )
+}
