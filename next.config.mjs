b4727636726b440f72109d6c2509a84cac86185d
AymanAkashi <aaggoujj@student.1337.ma>
2024-03-12 16:37:08 +0000

    feat: add shadcn and nextui

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        domains: ['https://source.unsplash.com'],
+    },
     webpack: (config, { isServer }) => {
         if (!isServer) {
             // set 'fs' to an empty module on the client to prevent this error on build --> Error: Can't resolve 'fs'
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,24 @@
 import type { Config } from 'tailwindcss'
+const { nextui } = require('@nextui-org/react')
 
-const config: Config = {
+const config = {
+    darkMode: ['class'],
     content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './pages/**/*.{ts,tsx}',
+        './components/**/*.{ts,tsx}',
+        './app/**/*.{ts,tsx}',
+        './src/**/*.{ts,tsx}',
+        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     ],
+    prefix: '',
     theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1400px',
+            },
+        },
         extend: {
             colors: {
                 background: '#FFFFFF',
@@ -15,6 +27,45 @@ const config: Config = {
                 accent: '#AFAFAF',
                 primary_dark: '#F9E800',
                 background_dark: '#121212',
+                border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                foreground: 'hsl(var(--foreground))',
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))',
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    foreground: 'hsl(var(--muted-foreground))',
+                },
+                popover: {
+                    DEFAULT: 'hsl(var(--popover))',
+                    foreground: 'hsl(var(--popover-foreground))',
+                },
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))',
+                },
+            },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)',
+            },
+            keyframes: {
+                'accordion-down': {
+                    from: { height: '0' },
+                    to: { height: 'var(--radix-accordion-content-height)' },
+                },
+                'accordion-up': {
+                    from: { height: 'var(--radix-accordion-content-height)' },
+                    to: { height: '0' },
+                },
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out',
             },
             backgroundImage: {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -28,13 +79,9 @@ const config: Config = {
             backgroundSize: {
                 'size-200': '200% 200%',
             },
-            backgroundPosition: {
-                'pos-0': '0% 0%',
-                'pos-100': '100% 100%',
-            },
         },
     },
-    plugins: [require('daisyui')],
-    darkMode: 'class',
-}
+    plugins: [require('tailwindcss-animate'), require('daisyui'), nextui()],
+} satisfies Config
+
 export default config
