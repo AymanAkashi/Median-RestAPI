f20f3a15637f2e9cedf5190a5b3012ac53c63dd1
AymanAkashi <aaggoujj@student.1337.ma>
2024-03-13 22:45:07 +0000

    feat: add lottiefiles animation for loading page

---
+++ app/loading.tsx
@@ -0,0 +1,19 @@
+'use client'
+import React from 'react'
+import Lottie from 'lottie-react'
+import { useEffect, useRef } from 'react'
+import animationData from '@/public/lottiefiles/LoadingBook.json'
+
+const Loading = () => {
+    return (
+        <div className="flex flex-col w-full justify-center items-center">
+            <Lottie
+                animationData={animationData}
+                className="flex justify-center items-center"
+                loop={true}
+            />
+        </div>
+    )
+}
+
+export default Loading
