818576a49b6b013daf2019cf76f32550365b96ed
AymanAkashi <aaggoujj@student.1337.ma>
2024-04-03 00:12:05 +0000

    fix: slide of the articles

--- app/home/NotifPage.tsx
+++ app/home/NotifPage.tsx
@@ -1,57 +1,54 @@
 'use client'
 import React from 'react'
+import Slides from './Slides'
 
 const NotifPage = () => {
-    const [switchSlides, setSwitchSlides] = React.useState([1, 2, 3])
+    const [switchSlides, setSwitchSlides] = React.useState(0)
 
     const handleSlide = (direction: string) => {
         if (direction === 'left') {
             setSwitchSlides((prev) => {
-                return prev.map((slide) => {
-                    return slide - 1 < 1 ? 3 : slide - 1
-                })
+                return prev - 1 < 0 ? 2 : prev - 1
             })
         } else {
             setSwitchSlides((prev) => {
-                return prev.map((slide) => {
-                    return slide + 1 > 3 ? 1 : slide + 1
-                })
+                return prev + 1 > 2 ? 0 : prev + 1
             })
         }
+        setSwitchSlides((prev) => {
+            console.log(prev)
+            return prev
+        })
     }
     const slides = [
         {
             title: 'Slide 1',
-            desription: 'This is the first slide',
+            description: 'This is the first slide',
             body: 'This is the body of the first slide',
-            image: 'https://via.placeholder.com/150',
+            image: 'https://source.unsplash.com/random/1280x720?sig=1',
         },
         {
             title: 'Slide 2',
-            desription: 'This is the second slide',
+            description: 'This is the second slide',
             body: 'This is the body of the second slide',
-            image: 'https://via.placeholder.com/150',
+            image: 'https://source.unsplash.com/random/1280x720?sig=1',
         },
         {
             title: 'Slide 3',
-            desription: 'This is the third slide',
+            description: 'This is the third slide',
             body: 'This is the body of the third slide',
-            image: 'https://via.placeholder.com/150',
+            image: 'https://source.unsplash.com/random/1280x720?sig=1',
         },
     ]
 
     return (
-        <div className="w-4/5 h-96 rounded-2xl bg-zinc-100 m-auto mt-6 flex justify-center relative overflow-x-scroll">
-            <div className="w-full h-96 bg-slate-500 absolute ">
-                <h1 className="text-2xl text-center">Notifications Page</h1>
-                <p className="text-center">This is the notification page</p>
-            </div>
-            <div className="w-full h-96 bg-yellow-500 absolute left-[100%]"></div>
-            <div className="w-full h-96 bg-purple-500 absolute left-[200%]"></div>
+        <div className="w-4/5 h-96 rounded-2xl bg-zinc-100 m-auto mt-6 flex justify-center relative">
+            <Slides Article={slides[switchSlides]} />
             <button
                 type="button"
                 title="switch Slide"
                 onClick={() => handleSlide('left')}
+                className="absolute left-1 top-[50%] transform font-bold text-2xl z-10 hover:text-primary_dark hover:scale-125 transition-all duration-150 delay-100"
             >
                 {'<'}
             </button>
@@ -59,6 +56,7 @@ const NotifPage = () => {
                 type="button"
                 title="switch Slide"
                 onClick={() => handleSlide('right')}
+                className="absolute right-1 top-[50%] transform font-bold text-2xl z-10 hover:text-primary_dark hover:scale-125 transition-all duration-150 delay-100"
             >
                 {'>'}
             </button>
--- app/home/Slides.tsx
+++ app/home/Slides.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Article } from '../types/types'
+import { DirectionAwareHover } from '@/components/ui/direction-aware-hover'
 
-const Slides = () => {
-    return <div></div>
+const Slides = ({ Article }: { Article: Partial<Article> }) => {
+    return (
+        <div className="h-full w-full">
+            <DirectionAwareHover
+                imageUrl={Article.image != undefined ? Article.image : ''}
+            >
+                <p className="font-bold text-2xl">{Article.title}</p>
+                <p className="text-lg">{Article.description}</p>
+            </DirectionAwareHover>
+        </div>
+    )
 }
 
 export default Slides
